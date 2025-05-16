434007ba92b91202d07c20f679024fa842401253
GustavoMartins2001 <59437683+GustavoMartins2001@users.noreply.github.com>
2025-05-16 17:37:11 +0000

    criado header

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from "react";
+import { createPortal } from 'react-dom';
+import axios from 'axios';
+import {
+  Button,
+  Container,
+  Form,
+  Row,
+  Col,
+  InputGroup,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+function MainPage() {
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+
+export default MainPage;
