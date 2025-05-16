dd987d89f9d168d7b34b9fbf7a330b781369f26d
GustavoMartins2001 <59437683+GustavoMartins2001@users.noreply.github.com>
2025-05-16 18:19:22 +0000

    criado card e mensagem de boas vindas

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -10,14 +10,27 @@ import {
   InputGroup,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import "./MainPage.css"
 
 function MainPage() {
 
   return (
-    <div className="welcome-message">
-      <h3>
-      </h3>
+    <div>
+      <div className="welcome-message text-center">
+      <h1>
+        Bem vindo à melhor calculadora de investimentos online!
+      </h1>
+      <h4 className="fitContent">Se você precisa calcular juros compostos, taxas, inflação ou mesmo
+        explorar os diferentes investimentos disponíveis no mercado, está no lugar
+        certo.
+      </h4> 
     </div>
+    <div className="whiteCard">
+      
+    </div>
+    </div>
+    
+    
   );
 }
 
