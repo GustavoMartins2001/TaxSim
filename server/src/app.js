a7e4ff2d46f617c7df973d0494b0f027a88fed57
unknown <igorbatistalima1@gmail.com>
2025-05-18 23:18:53 -0300

    Adicionado middleware de autenticação e ajustes no projeto

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./database/database');
+const userRoutes = require('./routes/userRoutes');
+
 
 connectDB();
 
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 module.exports = app;
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,3 +1,42 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const { User } = require('../models/User');
 
+const registerUser = async (req, res) => {
+  const { email, password, username } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, username, password: hashedPassword });
+    await user.save();
+    res.json({ message: 'Registration successful' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.find({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
